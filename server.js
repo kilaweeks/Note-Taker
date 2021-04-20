872ba369e3f16e80dcf6f691eb72645a378596d4
kiweeks <kilaweeks@gmail.com>
2021-04-20 16:59:30 -0700

    moved folders

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const express = require("express");
 const app = express();
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 
 // converts json to javascript
 app.use(express.json()); 
